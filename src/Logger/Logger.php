d1c4cf0dd4c974081d6e0e7e30684beb16182934
Tamas Mohos <zerosuxx@gmail.com>
2022-10-10 16:09:15 +0200

    remove unused import

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Onix\Logger;
 
-use DateTimeInterface;
 use Onix\Logger\Decorator\DecoratorInterface;
 use Onix\Logger\Decorator\TimestampDecorator;
 use Onix\Logger\Formatter\FormatterInterface;
